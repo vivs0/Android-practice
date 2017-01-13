6d5508e581e428d8e8eff0c6eec252fe42c84bdf
vivs0 <viveksingh0532@gmail.com>
2017-01-13 22:07:41 +0530

    update

---
+++ Android most basic/7-AndroidGridView/src/com/vivek/android/MyAndroidAppActivity.java
@@ -0,0 +1,17 @@
+package com.vivek.android;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.GridView;
+
+public class MyAndroidAppActivity extends Activity {
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+      
+      GridView gridview = (GridView) findViewById(R.id.gridview);
+      gridview.setAdapter(new ImageAdapter(this));
+   }
+}
